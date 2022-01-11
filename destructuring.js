4b33ee1eff567fa60ed99df01d7038b801267f29
brendanbutkus <brendan.butkus23@gmail.com>
2022-01-10 21:36:20 -0600

    first commit

---
+++ destructuring.js
@@ -0,0 +1,68 @@
+// const cars = ['Tesla', 'Mercedes', 'Honda']
+// const [ randomCar ] = cars
+// const [ ,otherRandomCar ] = cars
+// //Predict the output
+// console.log(randomCar)
+// console.log(otherRandomCar)
+
+// // Tesla then Mercedes
+// // does there need to be semi colons at the end?
+// // i guess not
+
+// const employee = {
+//     name: 'Elon',
+//     age: 47,
+//     company: 'Tesla'
+// }
+// const { name: otherName } = employee;
+// //Predict the output
+// console.log(name);
+// console.log(otherName);
+
+// Elon then Elon - i was wrong
+// reference error name is not defined - isn't name defined in the object?
+
+// const person = {
+//     name: 'Phil Smith',
+//     age: 47,
+//     height: '6 feet'
+// }
+// const password = '12345';
+// const { password: hashedPassword } = person;  
+// //Predict the output
+// console.log(password);
+// console.log(hashedPassword);
+// // the first password will print, but hashed password is making reference to an object without a password property?
+// // so it is not an error, it is just undefined
+
+const numbers = [8, 2, 3, 5, 6, 1, 67, 12, 2];
+const [,first] = numbers;
+const [,,,second] = numbers;
+const [,,,,,,,,third] = numbers;
+//Predict the output
+console.log(first == second);
+console.log(first == third);
+// the first is two and the second is five and the third is also two
+// the first will be false or null and the second console.log will be two
+// okay second console.log was true
+
+const lastTest = {
+    key: 'value',
+    secondKey: [1, 5, 1, 8, 3, 3]
+}
+const { key } = lastTest;
+const { secondKey } = lastTest;
+const [ ,willThisWork] = secondKey;
+//Predict the output
+console.log(key);
+console.log(secondKey);
+console.log(secondKey[0]);
+console.log(willThisWork);
+
+// we will get 'value'
+// then we will get the array
+// then 1
+// then 5
+
+
+
